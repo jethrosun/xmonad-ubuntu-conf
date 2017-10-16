61cf3f58de0ee4187838298be212a54acc263d29
shwsun <shwsun@bu.edu>
2017-10-16 10:46:00 -0400

    checkpoint: changed some files of fix an annoying bug, hooray

--- bin/adjust_brightness.sh
+++ bin/adjust_brightness.sh
@@ -8,7 +8,8 @@
 #   jethros ALL = (root) NOPASSWD: /home/jethros/.xmonad/bin/adjust_brightness.sh
 # 2. reboot
 
-# FIXME: this script will create a max_brightness file in ~/
+# FIXED: this script will create a max_brightness file in ~/
+cd $(dirname $0)
 
 cur_brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
 max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
