e0a107e6c3e26b6896646a4a97420f436ca009e0
shwsun <shwsun@bu.edu>
2018-03-30 15:38:35 -0400

    Update awesome

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-#killall -q polybar
+killall -q polybar
 
 # Wait until the processes have been shut down
 #while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-m=$(xrandr --query | grep " connected" | grep primary | cut -d" " -f1)
+if xrandr --query | grep " connected" | grep "DP-2" > /dev/null; then
+	m=$(xrandr --query | grep " connected" | grep "DP-2" | cut -d" " -f1)
+else
+	m=$(xrandr --query | grep " connected" | grep primary | cut -d" " -f1)
+fi
+
 cmd=$(env "MONITOR=$m" polybar --reload main)
 
 if [[ $# -gt 0 ]] && [[ $1 = "block" ]]; then
